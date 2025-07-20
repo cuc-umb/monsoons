004150bda2d78516a80f501eeb5ca282fb2e03f2
cuc-umb <zedd.fread@gmail.com>
2025-07-20 18:42:09 +0700

    move intensity and distance to lightning folder

--- src/controllers/LightingGUIController.ts
+++ src/controllers/LightingGUIController.ts
@@ -8,11 +8,12 @@ export class LightingGUIController {
   constructor(gui: GUI, sunlight: PointLight, helper: PointLightHelper) {
     this.updateLight = () => helper.update();
     this.gui = gui;
+
     const lightningFolder = this.gui.addFolder('lightning');
     lightningFolder.open()
 
-    this.gui.add(sunlight, 'intensity', 0, 1000, 1).onChange(this.updateLight);
-    this.gui.add(sunlight, 'distance', 0, 200).onChange(this.updateLight);
+    lightningFolder.add(sunlight, 'intensity', 0, 1000, 1).onChange(this.updateLight);
+    lightningFolder.add(sunlight, 'distance', 0, 200).onChange(this.updateLight);
 
     const positionFolder = lightningFolder.addFolder('position');
     positionFolder.add(sunlight.position, 'x', -20, 20).onChange(this.updateLight);
